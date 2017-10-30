86ee319245c0621ab0a45610af3c4c3e72eb231f
besark <besark@ttu.ee>
2017-10-30 15:09:09 +0200

    initialize the project
    spring boot project with h2 database is created.

---
+++ src/main/java/com/qminder/instadownloader/InstaDownloaderApplication.java
@@ -0,0 +1,12 @@
+package com.qminder.instadownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaDownloaderApplication.class, args);
+	}
+}
