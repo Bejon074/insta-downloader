7cce70a3241bb652ff5f3a22cd3756d438c37a6c
besark <besark@ttu.ee>
2017-11-01 00:28:09 +0200

    set pillingTimeGap to 12 min
    server will pull data from instagram in every 12 min

--- src/main/java/com/qminder/instadownloader/helper/Constants.java
+++ src/main/java/com/qminder/instadownloader/helper/Constants.java
@@ -4,5 +4,5 @@ public interface Constants {
 
     int downloadChunkSize = 20;
     int schedulerChunkSize = 100;
-    int poolingTimeGap = 7 * 60 * 1000;
+    int poolingTimeGap = 12 * 60 * 1000;
 }
